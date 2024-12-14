8cc26c1cb51bcec88eaf2cc35e3025bd0fe04469
Juye0nLee <dlwndus708@naver.com>
2024-12-14 20:16:12 +0900

    🎉 프로젝트 시작

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main/>}/>
+      </Routes>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
+
+
---
+++ src/pages/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import * as C from '../shared/style/ CommonStyle'
+
+export default function Main() {
+  return (
+    <C.Page>
+      <C.Center>
+          <C.PageSpace>
+            민주 청첩장
+          </C.PageSpace>
+      </C.Center>
+    </C.Page>
+  )
+}
