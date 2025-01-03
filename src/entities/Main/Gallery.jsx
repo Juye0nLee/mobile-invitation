624388119aefd7cc3eaa03716e0a1b093325e55a
Juye0nLee <dlwndus708@naver.com>
2025-01-03 13:25:36 +0900

    🎨 갤러리 수정

--- src/entities/Main/Gallery.jsx
+++ src/entities/Main/Gallery.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
+import Modal from 'react-modal'
 const images = [
   "https://jy-study-bucket.s3.ap-northeast-2.amazonaws.com/%E1%84%86%E1%85%A9%E1%84%8E%E1%85%A5%E1%86%BC/SJK_0568%E1%84%87%E1%85%A9%E1%84%8C%E1%85%A5%E1%86%BC.jpg",
   "https://jy-study-bucket.s3.ap-northeast-2.amazonaws.com/%E1%84%86%E1%85%A9%E1%84%8E%E1%85%A5%E1%86%BC/SJK_0577%E1%84%87%E1%85%A9%E1%84%8C%E1%85%A5%E1%86%BC.jpg",
@@ -22,8 +20,10 @@ const images = [
   "https://jy-study-bucket.s3.ap-northeast-2.amazonaws.com/%E1%84%86%E1%85%A9%E1%84%8E%E1%85%A5%E1%86%BC/SJK_0866%E1%84%87%E1%85%A9%E1%84%8C%E1%85%A5%E1%86%BC%E1%84%8C%E1%85%A2%E1%84%89%E1%85%AE%E1%84%8C%E1%85%A5%E1%86%BC.jpg",
   "https://jy-study-bucket.s3.ap-northeast-2.amazonaws.com/%E1%84%86%E1%85%A9%E1%84%8E%E1%85%A5%E1%86%BC/9F8191FF-26A6-41C1-8B9A-67512866A1A5.JPEG",
   "https://jy-study-bucket.s3.ap-northeast-2.amazonaws.com/%E1%84%86%E1%85%A9%E1%84%8E%E1%85%A5%E1%86%BC/IMG_0575.JPEG",
+  "https://jy-study-bucket.s3.ap-northeast-2.amazonaws.com/%E1%84%86%E1%85%A9%E1%84%8E%E1%85%A5%E1%86%BC/IMG_0705.JPEG",
   "https://jy-study-bucket.s3.ap-northeast-2.amazonaws.com/%E1%84%86%E1%85%A9%E1%84%8E%E1%85%A5%E1%86%BC/SJK_0977%E1%84%87%E1%85%A9%E1%84%8C%E1%85%A5%E1%86%BC.jpg",
   "https://jy-study-bucket.s3.ap-northeast-2.amazonaws.com/%E1%84%86%E1%85%A9%E1%84%8E%E1%85%A5%E1%86%BC/SJK_1013%E1%84%87%E1%85%A9%E1%84%8C%E1%85%A5%E1%86%BC%E1%84%92%E1%85%A1%E1%86%B8%E1%84%89%E1%85%A5%E1%86%BC.jpg",
+  "https://jy-study-bucket.s3.ap-northeast-2.amazonaws.com/%E1%84%86%E1%85%A9%E1%84%8E%E1%85%A5%E1%86%BC/SJK_0725%E1%84%87%E1%85%A9%E1%84%8C%E1%85%A5%E1%86%BC.jpg",
   "https://jy-study-bucket.s3.ap-northeast-2.amazonaws.com/%E1%84%86%E1%85%A9%E1%84%8E%E1%85%A5%E1%86%BC/SJK_0730%E1%84%87%E1%85%A9%E1%84%8C%E1%85%A5%E1%86%BC.jpg",
   "https://jy-study-bucket.s3.ap-northeast-2.amazonaws.com/%E1%84%86%E1%85%A9%E1%84%8E%E1%85%A5%E1%86%BC/SJK_0696%E1%84%87%E1%85%A9%E1%84%8C%E1%85%A5%E1%86%BC%E1%84%8C%E1%85%A2%E1%84%89%E1%85%AE%E1%84%8C%E1%85%A5%E1%86%BC.jpg",
   "https://jy-study-bucket.s3.ap-northeast-2.amazonaws.com/%E1%84%86%E1%85%A9%E1%84%8E%E1%85%A5%E1%86%BC/SJK_0680%E1%84%87%E1%85%A9%E1%84%8C%E1%85%A5%E1%86%BC.jpg",
@@ -32,16 +32,44 @@ const images = [
 ];
 
 export default function Gallery() {
-  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
-  const [isMoreView, setIsMoreView] = useState(false); // 더보기/접기 상태
+  const [selectedImage, setSelectedImage] = useState();
+  const [currentImage, setCurrentImage] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
 
-  const onClickImageMoreViewButton = () => {
-    setIsMoreView(!isMoreView); // 상태 반전
+
+  const handleThumbnailClick = (index) => {
+    setCurrentImage(index);
   };
 
-  const onclickImage = (index) => {
-    setSelectedImageIndex(index); // 선택된 이미지 인덱스 변경
+  const handleNext = () => {
+    setCurrentImage((prev) => (prev + 1) % images.length);
   };
+
+  const handleBack = () => {
+    setCurrentImage((prev) => (prev - 1 + images.length) % images.length);
+  };
+
+  const openModal = (image) => {
+    setIsOpen(true);
+    setSelectedImage(image)
+  };
+
+  const closeModal = () => setIsOpen(false);
+
+  const showModal = () => {
+    return (
+      <StyledModal
+        isOpen={isOpen}
+        onRequestClose={closeModal}
+        shouldCloseOnOverlayClick={false}
+      >
+        <CloseButton onClick={closeModal}>X</CloseButton>
+        <ModalContent>
+          <img src={selectedImage}/>
+        </ModalContent>
+      </StyledModal>
+    );
+  }
 
   return (
     <MainLayout>
@@ -54,35 +82,39 @@ export default function Gallery() {
       <StyledText color="#755D5D" fontSize="12px" marginBottom="32px">
         이미지를 클릭하시면 확대보기가 가능합니다.
       </StyledText>
-      <ImageContentWrap>
-        <MainImageWrap>
-          <MainImage
-            src={images[selectedImageIndex]}
-            alt="Selected Image"
-          />
-        </MainImageWrap>
 
-        {isMoreView === false && (
-            <WhiteGradientOverlay /> /* 버튼을 누르지 않았을 때만, 그라데이션 추가  */
-          )}
-
-        <PreviewImageWrap isMoreView={isMoreView}>
-          {images.slice(0, isMoreView ? images.length : 12).map((image, index) => (
-            <PreviewImage
+      <SliderContainer>
+        <Images>
+          {images.map((image, index) => (
+            <Image
               key={index}
               src={image}
-              alt={`Image ${index}`}
-              onClick={() => onclickImage(index)}
+              alt={`Slide ${index}`}
+              isActive={index === currentImage}
+              onClick={()=>openModal(image)}
             />
           ))}
-        </PreviewImageWrap>
+        </Images>
+        <Button className="back-btn" onClick={handleBack}>
+          <ion-icon name="chevron-back-outline"></ion-icon>
+        </Button>
+        <Button className="next-btn" onClick={handleNext}>
+          <ion-icon name="chevron-forward-outline"></ion-icon>
+        </Button>
+        <Thumbnails>
+          {images.map((image, index) => (
+            <Thumbnail
+              key={index}
+              src={image}
+              alt={`Thumbnail ${index}`}
+              isActive={index === currentImage}
+              onClick={() => handleThumbnailClick(index)}
+            />
+          ))}
+        </Thumbnails>
+      </SliderContainer>
 
-        {images.length > 12 && (
-          <PlusButton onClick={onClickImageMoreViewButton}>
-            {isMoreView ? "접기" : "더보기"}
-          </PlusButton>
-        )}
-      </ImageContentWrap>
+      {showModal()}
     </MainLayout>
   );
 }
@@ -112,62 +144,114 @@ const Line = styled.div`
   background-color: #755D5D;
   margin-bottom: ${({ marginBottom }) => marginBottom || "0px"};
 `;
-const ImageContentWrap = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: 20px;
-`;
-const MainImage = styled.img`
-  border-radius: 10px;
-`;
 
-const MainImageWrap = styled.div`
+const SliderContainer = styled.div`
+  position: relative;
   width: 80%;
-  display: flex;
-  justify-content: center;
-  margin-bottom: 10px;
-`;
-
-const PreviewImageWrap = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  max-height: ${(props) => (props.isMoreView ? "1000px" : "300px")};
+  height: 420px;
+  margin: 0 auto;
   overflow: hidden;
-  transition: max-height 0.2s ease-in-out;
-  margin-bottom: 10px;
-`;
-
-const PreviewImage = styled.img`
-  width: 80px;
-  height: 80px;
-  object-fit: cover;
-  margin: 5px;
   border-radius: 5px;
-  cursor: pointer;
+  box-shadow: 0px 50px 100px rgba(0, 0, 0, 0.4);
 `;
 
-const PlusButton = styled.div`
-  display: flex;
-  width: 100%;
-  padding: 10px 0;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-  margin-top: 10px;
-  z-index : 100;
-  font-family: GangwonEdu_OTFLightA;
-font-size: 16px;
-color: #755D5D;
-`;
-
-const WhiteGradientOverlay = styled.div`
+const Images = styled.div`
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  background: linear-gradient(rgba(253, 252, 245, 0) 0%, /* 그라데이션 설정 */
-      rgb(253, 252, 245) 100%);
 `;
+
+const Image = styled.img`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  opacity: ${(props) => (props.isActive ? 1 : 0)};
+  transform: ${(props) => (props.isActive ? "scale(1)" : "scale(1.1)")};
+  transition: all 500ms ease-in-out;
+`;
+
+const Button = styled.div`
+  position: absolute;
+  top: 0;
+  width: 40px;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  color: #fff;
+  font-size: 25px;
+  background: rgba(0, 0, 0, 0.2);
+  transition: background 300ms ease-in-out;
+
+  &:hover {
+    background: rgba(0, 0, 0, 0.5);
+  }
+
+  &.back-btn {
+    left: 0;
+  }
+
+  &.next-btn {
+    right: 0;
+  }
+`;
+
+const Thumbnails = styled.div`
+  position: absolute;
+  bottom: 5px;
+  width: 100%;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  gap: 5px;
+`;
+
+const Thumbnail = styled.img`
+  width: 60px;
+  height: auto;
+  cursor: pointer;
+  border: 2px solid ${(props) => (props.isActive ? "#fff" : "transparent")};
+  transition: border 300ms ease-in-out;
+`;
+
+const StyledModal = styled(Modal)`
+  width: 362px;
+  height: 338px;
+  padding: 0;
+  border: none;
+  background: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  margin: auto;
+  border-radius: 8px;
+  @media (max-width: 480px) {
+    width: 90%;
+    height: auto;
+    padding: 20px;
+  }
+`;
+
+const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background: none;
+  border: none;
+  font-size: 16px;
+  cursor: pointer;
+`;
+
